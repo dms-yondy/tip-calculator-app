220de3b71d426abfadea256fb9fbae416acf7747
Yondy <rprice1858@gmail.com>
2023-01-27 13:44:01 +0000

    removed git workflow and updated package.json

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dms-yondy.github.io/frontend-mentor/tip-calculator-app-main/.git main:gh-pages
+git push -f git@github.com:dms-yondy.github.io/tip-calculator-app-main/.git main:gh-pages
 
 cd -
