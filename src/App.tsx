e0e2ae46ff766965a6c90d3fb8c4b31f356216f7
Yondy <rprice1858@gmail.com>
2023-02-06 15:53:34 +0000

    added main tag for app

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import TipCalculator from "./components/TipCalculator";
 
 function App() {
   return (
-    <div className="app-container">
+    <main className="app-container">
       <h1 className="app-title">
         <span className="app-title-top">SPLI</span>
         <span className="app-title-bottom">TTER</span>
       </h1>
       <TipCalculator />
-    </div>
+    </main>
   );
 }
 
